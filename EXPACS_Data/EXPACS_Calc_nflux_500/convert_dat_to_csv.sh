ab971c2a489c46273ea9e1e3c6a1d154911477f2
Franco Lopez <franco.lopez@ug.uchile.cl>
2024-02-19 14:30:23 -0300

    Directories and files wihtin that are genetared from functions.

---
+++ EXPACS_Data/EXPACS_Calc_nflux_500/convert_dat_to_csv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Loop through files from LasCampanasAtacama_00.dat to LasCampanasAtacama_100.dat
+for i in {0..150..1}; do
+    input_file="./LasCampanasAtacama_${i}.dat"
+    output_file="./csv_files/LasCampanasAtacama_${i}.csv"
+
+    # Check if the input file exists
+    if [ -e "$input_file" ]; then
+        # Add header and replace spaces with commas
+        echo "lower_edge_binvalue,bin_width,flux_value" > "$output_file"
+        # Cut the input file to contain only 130 rows and replace spaces with commas
+        head -n 130 "$input_file" | sed 's/ /,/g' >> "$output_file"
+
+        echo "Conversion complete for $input_file. Output saved to $output_file"
+    else
+        echo "Input file $input_file not found. Skipping..."
+    fi
+done
