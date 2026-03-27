b85e4eb5842b243c25d0ff452b8eb1a966bcfa57
Franco Lopez <franco.lopez@ug.uchile.cl>
2026-03-27 18:07:55 +0000

    Added EM config files for Maricunga (15 min)

--- configs/unfolding_configs/config_EM_MC_stop_Maricunga_15min_BEAM.sh
+++ configs/unfolding_configs/config_EM_MC_stop_Maricunga_15min_BEAM.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # ==== CONFIGURACIÓN DE PARÁMETROS FÍSICOS ====
-CAMPAIGN="Chapiquilta"  # nombre de campaña
+CAMPAIGN="Maricunga"  # nombre de campaña
 STEPS=0                 # EM con criterio de stop
-TIME_GRID=60            # tamaño de la grilla de tiempo en min
+TIME_GRID=15            # tamaño de la grilla de tiempo en min
 NDET=11                 # número de detectores
 MAX_STEPS=100           # máximo de steps EM
 BIN_SEED=1              # 1 (random seed)
 CUT="cut1"              # nombre del corte
 PHYLST="FTFP_BERT"      # Response Functions Physics List used in geant4 sim (QGSP_BERT or FTFP_BERT)
 SCF="ws"                # SCALE FACTOR, ws: incluye 1/4 en RF, wos: no incluye 1/4 en RF
-NEUFTY="ISO"           # ISO, MIX o BEAM, Tipo de hipotesis para el campo  neutronico usado en el calculo de RF
+NEUFTY="BEAM"           # ISO, MIX o BEAM, Tipo de hipotesis para el campo  neutronico usado en el calculo de RF
 
 # ==== CONFIGURACIÓN DEL ARRAY ====
-TOTAL_EVENTS=838
+TOTAL_EVENTS=342
 MAX_PARALLEL=88
-EXCLUDED_EVENTS="85 86 87 88 150 630 631 632 633 634 635 636 757 837 838"
+EXCLUDED_EVENTS=" "
