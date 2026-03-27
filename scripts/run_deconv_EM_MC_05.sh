1965340bf9ac2e37e50ca13504100aa6d6f05147
Franco Lopez <franco.lopez@ug.uchile.cl>
2026-03-27 19:39:16 +0000

    Deleted script files

--- scripts/run_deconv_EM_MC_05.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-root -b <<EOC
-.L deconv_CRNS.C
-em_loop_events_MC_opt("RetenDesierto",93,93,0,15,11,42,1,"cut1")
-.q
-EOC
-
--- scripts/run_deconv_EM_MC_06.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-root -b <<EOC
-.L deconv_CRNS.C
-em_loop_events_MC_opt("RetenDesierto",94,96,0,15,11,42,1,"cut1")
-.q
-EOC
-
